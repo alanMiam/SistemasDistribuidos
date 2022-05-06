ed45313e11174d523b73ac1728b067028cb94272
Alan Miam <89094977+alanMiam@users.noreply.github.com>
2022-05-06 11:13:47 -0500

    Add files via upload

---
+++ comunicacionProcesosLocales/SocketEscritor/src/main/java/SocketCliente/Main.java
@@ -0,0 +1,11 @@
+package SocketCliente;
+/**
+ *
+ * @author Fam.Campos
+ */
+public class Main {
+    public static void main(String[]args){
+        Conector c = new Conector();
+        c.inicio();
+    }
+}
